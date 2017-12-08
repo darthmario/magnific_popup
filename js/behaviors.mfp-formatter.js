bd725410508afb6940e5177566c13aa626158734
Jay Dansand <dansandj@lawrence.edu>
2017-12-08 11:09:09 -0600

    YouTube support improvements: better error detection in video_ID regex, support non-standard-order /watch URLs, support "time_continue" parameter.

--- js/behaviors.mfp-formatter.js
+++ js/behaviors.mfp-formatter.js
@@ -17,14 +17,16 @@
     fixedContentPos: 'auto'
   };
   var youtube_id = function (url) {
-    // Get video ID and extra params (if they exist).
+    // Get video ID and extra params (if they exist). Sometimes the order of
+    // parameters in "/watch?"-style URLs can change, so allow "v=" to appear
+    // anywhere.
     // Example URLs:
     //  http://www.youtube.com/watch?v=VIDEOID&t=60&list=PLAYLISTID
     //  https://www.youtube.com/v/VIDEOID?t=60&list=PLAYLISTID
     //  https://www.youtube.com/embed/VIDEOID?t=60&list=PLAYLISTID
     //  https://youtu.be/VIDEOID?t=60&list=PLAYLISTID
-    var video_ID = /(youtu\.be\/|youtube\.com\/(embed|v)\/|watch\?v=)([a-zA-Z0-9_-]+)/.exec(url);
-    if (typeof video_ID[3] === 'undefined') {
+    var video_ID = /(youtu\.be\/|youtube\.com\/(embed|v)\/|watch\?v=|watch\?.*&v=)([a-zA-Z0-9_-]+)/.exec(url);
+    if (!video_ID || !video_ID.length || typeof video_ID[3] === 'undefined') {
       return false;
     }
     var video_params_test = /\?+(.+)/.exec(url), video_params = '';
@@ -34,8 +36,8 @@
       video_params = video_params.replace(/&v=.*?(?=$|&)/gi, '');
       // Remove autoplay param, if it exists.
       video_params = video_params.replace(/&autoplay=.*?(?=$|&)/gi, '');
-      // Convert "t" to "start" param.
-      video_params = video_params.replace(/&t=/gi, '&start=');
+      // Convert "t"/"time_continue" to "start" param.
+      video_params = video_params.replace(/&(t|time_continue)=/gi, '&start=');
       // Adjust "start" param; the YT share GUI generates URLs like ?t=1h2m3s
       // but the embed endpoint requires time in seconds with no units or text
       // chars of any kind.
