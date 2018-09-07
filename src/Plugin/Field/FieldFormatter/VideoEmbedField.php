d6831976556bda2c971432ebac3763bbf048139b
Eric115 <Eric115@2877061.no-reply.drupal.org>
2018-09-07 21:52:24 +0800

    Issue #2794479 by Eric115, anish.a, stewest: You have requested a non-existent service "colorbox.attachment"

--- src/Plugin/Field/FieldFormatter/VideoEmbedField.php
+++ src/Plugin/Field/FieldFormatter/VideoEmbedField.php
@@ -2,11 +2,15 @@
 
 namespace Drupal\magnific_popup\Plugin\Field\FieldFormatter;
 
-use Drupal\video_embed_field\Plugin\Field\FieldFormatter\Colorbox;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FormatterInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Render\RendererInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Component\Utility\Html;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Magnific Popup FieldFormatter for Video Embed Field.
@@ -19,7 +23,78 @@ use Drupal\Component\Utility\Html;
  *   }
  * )
  */
-class VideoEmbedField extends Colorbox {
+class VideoEmbedField extends FormatterBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The field formatter plugin instance for thumbnails.
+   *
+   * @var \Drupal\Core\Field\FormatterInterface
+   */
+  protected $thumbnailFormatter;
+
+  /**
+   * The field formatter plugin instance for videos.
+   *
+   * @var \Drupal\Core\Field\FormatterInterface
+   */
+  protected $videoFormatter;
+
+  /**
+   * The renderer.
+   *
+   * @var \Drupal\Core\Render\RendererInterface
+   */
+  protected $renderer;
+
+  /**
+   * Constructs a new instance of the plugin.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the formatter.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the formatter is associated.
+   * @param array $settings
+   *   The formatter settings.
+   * @param string $label
+   *   The formatter label display setting.
+   * @param string $view_mode
+   *   The view mode.
+   * @param array $third_party_settings
+   *   Third party settings.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   The renderer.
+   * @param \Drupal\Core\Field\FormatterInterface $thumbnail_formatter
+   *   The field formatter for thumbnails.
+   * @param \Drupal\Core\Field\FormatterInterface $video_formatter
+   *   The field formatter for videos.
+   */
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, RendererInterface $renderer, FormatterInterface $thumbnail_formatter, FormatterInterface $video_formatter) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
+    $this->thumbnailFormatter = $thumbnail_formatter;
+    $this->videoFormatter = $video_formatter;
+    $this->renderer = $renderer;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $formatter_manager = $container->get('plugin.manager.field.formatter');
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('renderer'),
+      $formatter_manager->createInstance('video_embed_field_thumbnail', $configuration),
+      $formatter_manager->createInstance('video_embed_field_video', $configuration)
+    );
+  }
 
   /**
    * {@inheritdoc}
